9cc1412c42f0e64604dbe96c214b1682bb54ebb0
Brian Mosley & Sneha Narasimhan <pair+brianlmosley.snehabn@devbootcamp.com>
2016-05-26 16:27:15 -0700

    Add post method to route back to root if the user is created.

--- app/controllers/user.rb
+++ app/controllers/user.rb
@@ -2,4 +2,12 @@ get '/users/new' do
   erb :'users/new'
 end
 
+post '/users/new' do
+  @user = User.new(params[:user])
+  if @user.save
+    redirect '/'
+  else
+    erb :'users/new'
+  end
+end
 
