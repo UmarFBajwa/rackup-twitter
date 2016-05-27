2c807b3c75c08d85234dff1a5a7beb8ad5ee26be
Brian Mosley & Sneha Narasimhan <pair+brianlmosley.snehabn@devbootcamp.com>
2016-05-26 21:39:41 -0700

    Add ability to show tweets on individual users page

--- app/controllers/tweets.rb
+++ app/controllers/tweets.rb
@@ -1,17 +1,20 @@
+get '/tweets' do
+  @tweet = Tweet.all
+end
+
 get '/users/:id/tweets' do
   @user = User.find(params[:id])
-  @tweets = @user.tweets
   erb :'tweets/show'
 end
 
-
-
 post '/users/:id/tweets' do
   @user = User.find(params[:id])
-  @tweet = Tweet.new(body: params[:body])
+  @tweet = Tweet.new(body: params[:body], user_id: @user.id)
   if @tweet.save
     redirect "/users/#{@user.id}/tweets"
   else
     redirect "/users/show"
   end
 end
+
+
