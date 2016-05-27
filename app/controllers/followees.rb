a74600788975d9969cfbf243c9c5d5253c4dac4e
Umar Farooq Bajwa & Olya Royall <pair+UmarFBajwa.venture-vin@devbootcamp.com>
2016-05-27 15:00:40 -0700

    Add ability to follow others, but not yourself or people that are already followed

--- app/controllers/followees.rb
+++ app/controllers/followees.rb
@@ -3,3 +3,16 @@ get '/followees/:id' do
   @followings = @user.followees
   erb :'following/show'
 end
+
+post '/followees/:id' do
+  @user = User.find(params[:id])
+  new_follow = Following.new(follower_id: session[:user_id], followee_id: @user.id)
+  if session[:user_id] == @user.id
+    redirect "/users/#{@user.id}/tweets"
+  elsif Following.where(follower_id: session[:user_id], followee_id: @user.id)
+    redirect "/users/#{@user.id}/tweets"
+  else
+    new_follow.save
+  end
+
+end
