c52d47b92c7422e4c366bb5d6d17080ea8df8154
Brian Mosley & Sneha Narasimhan <pair+brianlmosley.snehabn@devbootcamp.com>
2016-05-26 18:51:34 -0700

    Revise post route for login and add logout route.

--- app/controllers/registration.rb
+++ app/controllers/registration.rb
@@ -4,13 +4,20 @@
 # end
 
 
-post '/login/' do
-  erb :"users/show"
+post '/login' do
+  user = User.authenticate(params[:username], params[:password])
+  if user
+    session[:user_id] = user.id
+    erb :"users/#{user.id}"
+  else
+    redirect '/'
+  end
 end
 
 
 get '/logout' do
-
+  session.clear
+  redirect '/'
 end
 
 get '/register' do
@@ -19,6 +26,5 @@ end
 
 post '/register' do
   user = User.create(params[:user])
-  session[:user_id] = user.id
   redirect '/'
 end
