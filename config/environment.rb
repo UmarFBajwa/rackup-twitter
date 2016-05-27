13ff2a1b0182e55219b33617cbc06910224fab80
Umar Farooq Bajwa & Olya Royall <pair+UmarFBajwa.venture-vin@devbootcamp.com>
2016-05-27 16:21:25 -0700

    Readded bcrypt

--- config/environment.rb
+++ config/environment.rb
@@ -7,6 +7,7 @@ require 'bundler/setup' if File.exists?(ENV['BUNDLE_GEMFILE'])
 
 # Require gems we care about
 require 'rubygems'
+require 'bcrypt'
 
 require 'uri'
 require 'pathname'
@@ -20,7 +21,6 @@ require 'sinatra'
 require "sinatra/reloader" if development?
 
 require 'erb'
-# require 'bcrypt'
 
 # Some helper constants for path-centric logic
 APP_ROOT = Pathname.new(File.expand_path('../../', __FILE__))
