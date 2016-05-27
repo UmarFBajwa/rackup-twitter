1ab31122cf0972ae6f0610e4acf97c9d2f49e721
Brian Mosley & Sneha Narasimhan <pair+brianlmosley.snehabn@devbootcamp.com>
2016-05-26 18:25:56 -0700

    Add post route to registration and creates session with user id.

--- app/controllers/registration.rb
+++ app/controllers/registration.rb
@@ -14,9 +14,11 @@ get '/logout' do
 end
 
 get '/register' do
-
+  erb :"users/new"
 end
 
 post '/register' do
-
+  user = User.create(params[:user])
+  session[:user_id] = user.id
+  redirect '/'
 end
