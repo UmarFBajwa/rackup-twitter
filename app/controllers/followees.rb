154e753d62b5ee7b68c1860223a74953dfab5bf8
Umar Farooq Bajwa & Olya Royall <pair+UmarFBajwa.venture-vin@devbootcamp.com>
2016-05-27 15:28:39 -0700

    update follow/unfollow

--- app/controllers/followees.rb
+++ app/controllers/followees.rb
@@ -6,19 +6,21 @@ end
 
 post '/followees/:id' do
   @user = User.find(params[:id])
-  new_follow = Following.new(follower_id: session[:user_id], followee_id: @user.id)
+
   if session[:user_id] == @user.id
     redirect "/users/#{@user.id}/tweets"
-  elsif Following.where(follower_id: session[:user_id], followee_id: @user.id)
+  elsif !(Following.where(follower_id: session[:user_id], followee_id: @user.id).empty?)
     redirect "/users/#{@user.id}/tweets"
   else
+    new_follow = Following.new(follower_id: session[:user_id], followee_id: @user.id)
     new_follow.save
   end
+  redirect "/followees/#{session[:user_id]}"
 end
 
 delete '/followees/:id' do
   @user = User.find(params[:id])
   destroy_follow = Following.find_by(follower_id: session[:user_id], followee_id: @user.id)
   destroy_follow.destroy
-    redirect "/users/#{@user.id}/tweets"
+    redirect "/followees/#{session[:user_id]}"
 end
